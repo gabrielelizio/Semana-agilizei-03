d3a012e447f18246615603690d20b46d949e0db1
Gabriel Elizio <gabrielelizio@hotmail.com>
2023-06-26 16:40:47 -0300

    Finalização projeto dia 3

--- cypress/e2e/finances.specy.cy.js
+++ cypress/e2e/finances.specy.cy.js
@@ -1,14 +1,21 @@
+/// <reference types="cypress"/>
 
+import {format,prepareLocalStorage} from '../support/utils'
 
 context('Dev Finances Agilizei', () => {
 
 
   beforeEach(() => {
-    cy.visit ('https://dev-finance.netlify.app') //visitando a pagina
-     cy.get('#data-table tbody tr').should('have.length',0)
+    cy.visit ('https://dev-finance.netlify.app',{
+    onBeforeLoad: (win) =>{
+      prepareLocalStorage(win)
+    }
+     //visitando a pagina
+     })
+     
+     
+    //  cy.get('#data-table tbody tr').should('have.length',0)
   });
-
-
    it('Cadastrar entradas', () => {
      cy.get('#transaction > .button').click() // clicando para uma nova transação
      cy.get('#description').type('Presente') // escrevendo o tipo da nova transação
@@ -16,7 +23,7 @@ context('Dev Finances Agilizei', () => {
      cy.get('#date').type('2023-06-21') //preenchendo a data da transação
      cy.get('button').contains('Salvar').click() //salvando a transação
      
-     cy.get('#data-table').should('have.length',1)
+     cy.get('#data-table tbody tr').should('have.length',3)
 
 
     }); 
@@ -28,8 +35,7 @@ context('Dev Finances Agilizei', () => {
       cy.get('#date').type('2023-06-21') //preenchendo a data da transação
       cy.get('button').contains('Salvar').click() //salvando a transação
       
-      cy.get('#data-table').should('have.length',1)
- 
+      cy.get('#data-table tbody tr').should('have.length',3)
  
      }); 
 
@@ -37,20 +43,7 @@ context('Dev Finances Agilizei', () => {
 
 
       const entrada = 'Mesada'
-      const saida = 'KinderOvo'
-
-
-      cy.get('#transaction > .button').click() 
-      cy.get('#description').type(entrada) 
-      cy.get('#amount').type(1000) 
-      cy.get('#date').type('2023-06-21') 
-      cy.get('button').contains('Salvar').click() 
-
-      cy.get('#transaction > .button').click() 
-      cy.get('#description').type(saida) 
-      cy.get('#amount').type(-35) 
-      cy.get('#date').type('2023-06-21') 
-      cy.get('button').contains('Salvar').click() 
+      const saida = 'Suco Kapo'
 
       //1ª estrategia para excluir a entrada. 
       //acessando a descrição para pegar a imagem da linha que contem a descrição
@@ -60,7 +53,6 @@ context('Dev Finances Agilizei', () => {
       .parent()
       .find('img[onclick*=remove]')
       .click()
-      //  cy.get('#totalDisplay').should('equal','**R$35**')
       //2ª Estrategia buscar todos os irmãos e buscar o que tem a img + atributo
 
       cy.get('td.description')
@@ -73,9 +65,45 @@ context('Dev Finances Agilizei', () => {
     
     });
 
+    it('Validar Saldo com diversas transações', () => {
+   
+    
+      //Definindo as variaveis de entrada e saída para inicilizar com 0
+
+      let incomes = 0
+      let expenses = 0
+      //pegando o get do data-table para listar todos elementos da tela
+      // e usar o invoke para pegar o texto e formatar
+      cy.get("#data-table tbody tr")
+        .each(($el, index, $list ) =>{
+          cy.get($el)
+          .find('.expense, .income')
+          .invoke( 'text' )
+          .then(text => {
+            if(text.includes('-')){
+              expenses = expenses +format(text)
+              cy.log(expenses)
+            }          else {
+              incomes = incomes+format(text)
+            }
+            
+          })
+          
+        })
+
+          //função para verificar se o total encontrado no datable é igual ao valor total
+
+          cy.get('#totalDisplay').invoke('text').then(text =>{
+
+            let formattedTotalDisplay = format(text)
+            let expectedTotal = expenses +incomes
 
 
-
+            expect(expectedTotal).to.eq(formattedTotalDisplay)
+        })
+        
+      
+    });
 
 
 });
